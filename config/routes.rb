9ac915fab8ae35cd0fefd340afa745314d33c9f6
Paul Chavinier <paul.chavinier@gmail.com>
2016-05-24 11:01:40 +0200

    route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations", omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+  get 'searchmap', to: 'pages#searchmap'
 
   resources :guides
   resources :cards
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
