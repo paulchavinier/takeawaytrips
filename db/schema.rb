ba836a093487c17b319f8cbe7b46fb208c3f6b12
Paul Chavinier <paul.chavinier@gmail.com>
2016-05-25 17:42:38 +0200

    cards can be pinned on map

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525084821) do
+ActiveRecord::Schema.define(version: 20160525134632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 20160525084821) do
     t.integer  "guide_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.string   "photo"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "photo"
   end
 
   add_index "cards", ["guide_id"], name: "index_cards_on_guide_id", using: :btree
