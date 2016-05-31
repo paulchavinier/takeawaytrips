3da5a2cb600c7c7dc410c385a5bed73e7cf43a78
Paul Chavinier <paul.chavinier@gmail.com>
2016-05-31 13:51:03 +0200

    new dv

--- db/schema.rb
+++ db/schema.rb
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 20160530212743) do
     t.integer  "guide_id"
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
-    t.string   "photo"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "photo"
     t.string   "rating"
     t.string   "gphoto"
     t.boolean  "open_now"
