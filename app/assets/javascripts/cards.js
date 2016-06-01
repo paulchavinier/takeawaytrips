4059acbe58706e3d133060e9c6510bfb2d8d0a9c
Constant Desfosse <desfosse.constant@gmail.com>
2016-06-01 17:52:26 +0200

    ok

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $(".showhours").click(function(){
-    $(this).next(".list-unstyled").toggle(500);
+    $(this).next(".list-unstyled").slideToggle(500);
   });
 });
 
 $(document).ready(function(){
   $(".shownote").click(function(){
-    $(this).next(".notes").toggle(500);
+    $(this).next(".notes").slideToggle(500);
   });
 });
